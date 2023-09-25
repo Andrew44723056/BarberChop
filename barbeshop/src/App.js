121903c49c4658ca73a07a47c4842be7388c1063
Tu nombre <ejemplo@email.com>
2023-09-25 10:21:07 -0600

    25/9

--- barbeshop/src/App.js
+++ barbeshop/src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
+import AppRouter from './router/AppRouter';
 
 function App() {
   return (
     <div className="App">
-      Hollaaa
-      holaaaaaaaaa222
-      holaaaaaaaaaaaaaaaaaaa
-
+     <AppRouter/>
     </div>
   );
 }
---
+++ barbeshop/src/router/AppRouter.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "../App.css";
+
+function AppRouter() {
+    return (
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About/>} />
+        </Routes>
+      </div>
+    );
+  }
+export default AppRouter;
